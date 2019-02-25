8ef3ea4c92b0dcf6575ba7f7bf35f3c9be25822f
romain sanson <romainsanson.gm@gmail.com>
2019-02-25 23:13:15 +0100

    tweaking calendar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,14 +10,28 @@
 
 const events = [
   { title: 'Event', start: '2019-02-27', end: '2019-03-01', color: 'red', editable: true },
-  { title: 'Event', start: '2019-02-03', end: '2019-02-04', color: 'blue', editable: true },
+  { title: 'Event', start: '2019-02-01', end: '2019-02-01', color: 'blue', editable: true },
   { title: 'Event', start: '2019-02-04', end: '2019-02-07', color: 'green', editable: true },
   { title: 'Event', start: '2019-02-14', end: '2019-02-16', color: 'purple', editable: true },
   { title: 'Event', start: '2019-02-15', end: '2019-02-16', color: 'black', editable: true },
+  { title: 'Event', start: '2019-02-07', end: '2019-02-08', color: 'orange', editable: true },
+  { title: 'Event', start: '2019-02-11', end: '2019-02-13', color: 'yellow', editable: true },
 ]
 
 $('#calendar').fullCalendar({
   locale: 'fr',
+  selectable: true,
+  header: {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'month,agendaWeek,agendaDay'
+  },
+  dayClick: function(date) {
+    alert('clicked ' + date.format());
+  },
+  select: function(startDate, endDate) {
+    alert('selected ' + startDate.format() + ' to ' + endDate.format());
+  }
 });
 
 function addEvent(event) {

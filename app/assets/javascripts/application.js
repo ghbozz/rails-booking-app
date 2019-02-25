c6d26f1db5d01573c7e4832c91c42a2d9446b34f
romain sanson <romainsanson.gm@gmail.com>
2019-02-25 22:30:32 +0100

    calendar tbc

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,6 +8,27 @@
 //= require fullcalendar
 //= require fullcalendar/locale-all
 
+const events = [
+  { title: 'Event', start: '2019-02-27', end: '2019-03-01', color: 'red', editable: true },
+  { title: 'Event', start: '2019-02-03', end: '2019-02-04', color: 'blue', editable: true },
+  { title: 'Event', start: '2019-02-04', end: '2019-02-07', color: 'green', editable: true },
+  { title: 'Event', start: '2019-02-14', end: '2019-02-16', color: 'purple', editable: true },
+  { title: 'Event', start: '2019-02-15', end: '2019-02-16', color: 'black', editable: true },
+]
+
 $('#calendar').fullCalendar({
-  locale: 'fr'
+  locale: 'fr',
 });
+
+function addEvent(event) {
+  $('#calendar').fullCalendar('renderEvent', event)
+}
+
+function injectEvents(events) {
+  events.forEach((event) => {
+    addEvent(event)
+  })
+}
+
+injectEvents(events);
+

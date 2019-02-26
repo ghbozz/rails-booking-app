c1cdfa7b30b35f8c09d580005909dc4bc2204a6a
romain sanson <romainsanson.gm@gmail.com>
2019-02-26 22:34:30 +0100

    daterangepicker tbc

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -47,16 +47,9 @@ if (document.getElementById('calendar')) {
 
 /////// DATE RANGE PICKER //////////
 
+
 $(document).ready(function() {
   $('.date_range').daterangepicker({
-    ranges: {
-       'Today': [moment(), moment()],
-       'Yesterday': [moment().subtract('days', 1), moment().subtract('days', 1)],
-       'Last 7 Days': [moment().subtract('days', 6), moment()],
-       'Last 30 Days': [moment().subtract('days', 29), moment()],
-       'This Month': [moment().startOf('month'), moment().endOf('month')],
-       'Last Month': [moment().subtract('month', 1).startOf('month'), moment().subtract('month', 1).endOf('month')]
-    },
     locale: { cancelLabel: 'Clear' },
     format: 'YYYY-MM-DD',
     opens: 'left'
